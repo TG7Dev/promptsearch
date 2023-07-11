26b282c49c0943ef80b3a8651409aa69eafa6ac3
TG7DEV <TG7Dev@gmail.com>
2023-07-11 20:55:54 +1000

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Feed from "@components/Feed";
+
+export const Home = () => {
+  return (
+   <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      Discover & Share
+      <br />
+      <span className="orange_gradient text-center">
+        AI-Powered Prompts </span>
+    </h1>
+    <p className="desc text-center">
+      Promptopia is an opensource AI to the modern world to discover, create and share creative prompts
+    </p>
+
+    <Feed/>
+   </section>
+
+  )
+}
+
+export default Home;
